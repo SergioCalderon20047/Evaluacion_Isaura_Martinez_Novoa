534d1db8c540cf365120c349c1bcdd48994c4107
Sergio Alejandro Calderon Perdomo <114233732+SergioCalderon20047@users.noreply.github.com>
2023-09-07 09:12:16 -0500

    Add files via upload
    Codigo Realizado

---
+++ Codigo05.js
@@ -0,0 +1,10 @@
+var vector1 = [1, 2, 3, 4, 5, 6, 8, 9, 10];
+var vector2 = [10, 20, 30, 40, 50, 60, 80, 90, 100];
+
+
+function suma_vec(vector1, vector2){
+    for(i=1; vector1>1; i++){
+        total_vec = vector1 + i++;
+        console.log(i);
+    }
+}
