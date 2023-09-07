7674ee35ad3f09598a4ab9878cf1ff7db8c3f3a4
Sergio Alejandro Calderon Perdomo <114233732+SergioCalderon20047@users.noreply.github.com>
2023-09-07 09:01:51 -0500

    Add files via upload
    Codigo Realizado

---
+++ Codigo04.js
@@ -0,0 +1,9 @@
+var numero1 = parseInt("Ingrese un numero: ");
+var numero2 = parseInt("Ingrese otro numero: ");
+
+function numero_primo(){
+    for(i=1; i>=1; i++){
+        numtotales.lengt(numero1, numero2);
+        console.log(numtotales)
+    }
+}
