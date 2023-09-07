f968122b2cc40fb07bf8efdf6f3913b004cae063
Sergio Alejandro Calderon Perdomo <114233732+SergioCalderon20047@users.noreply.github.com>
2023-09-07 08:28:09 -0500

    Add files via upload
    Primer Codigo

---
+++ Codigo01.js
@@ -0,0 +1,9 @@
+var nombre_empl = ["Eduardo", "Jose", "Camilo", "Santiago"]; 
+var sueldo = ["190000", "240000", "1100000", "300000"]
+
+
+for(i=1; i >=1; i++){
+    total_sueldo = sueldo.array[1++]
+    console.log(total_sueldo)
+}
+
